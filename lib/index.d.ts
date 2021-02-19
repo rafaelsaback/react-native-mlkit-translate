bc7136c6620115355faeeaad5b304f7243520000
Rafael Saback <rafaelsaback@gmail.com>
2021-02-19 16:14:25 +0100

    Commit transpiled types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 interface IMLKitTranslateModule {
     translate(text: string, sourceLanguage: string, targetLanguage: string): Promise<string>;
+    getDownloadedModels(): Promise<string[]>;
 }
 declare const _default: IMLKitTranslateModule;
 export default _default;
